4ba708c69aab6fa4891d79084a88aa6bc0acdde9
OhBeomho <tyler0322@naver.com>
2023-07-15 12:00:22 +0900

    init

---
+++ src/components/Layout.svelte
@@ -0,0 +1,21 @@
+<script>
+  const year = new Date().getFullYear()
+</script>
+
+<div class="wrapper">
+  <header>
+    <nav>
+      <a href="/" class="brand">coloring-text</a>
+      <div>
+        <a href="/docs">Docs</a>
+        <a href="/examples">Examples</a>
+      </div>
+    </nav>
+  </header>
+  <main>
+    <slot />
+  </main>
+  <footer>
+    <p>Copyright {year}. OhBeomho.</p>
+  </footer>
+</div>
